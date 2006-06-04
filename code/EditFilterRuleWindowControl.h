c2849fd32ba48dc63909403ea5835b6c74d3cfd6
erwin <erwin@3e1cca9a-9203-0410-b20e-959a89a76e17>
2006-06-04 14:54:33 +0000

    Made EditFilterRuleWindow more powerful.
    git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@85 3e1cca9a-9203-0410-b20e-959a89a76e17

--- code/EditFilterRuleWindowControl.h
+++ code/EditFilterRuleWindowControl.h
@@ -4,14 +4,23 @@
 @interface EditFilterRuleWindowControl : NSWindowController {
 
   IBOutlet NSTextField  *ruleNameField;
-
-  IBOutlet NSPopUpButton  *ruleTargetButton;
-  IBOutlet NSPopUpButton  *ruleTestButton;
   
-  IBOutlet NSTextView  *ruleTargetsView;
+  IBOutlet NSButton  *typeCheckBox;
+  IBOutlet NSPopUpButton  *typePopUpButton;
+
+  IBOutlet NSButton  *nameCheckBox;
+  IBOutlet NSPopUpButton  *nameMatchPopUpButton;
+  IBOutlet NSTextView  *nameTargetsView;
+
+  IBOutlet NSButton  *sizeLowerBoundCheckBox;
+  IBOutlet NSTextField  *sizeLowerBoundField;
+  IBOutlet NSPopUpButton  *sizeLowerBoundUnits;
+    
+  IBOutlet NSButton  *sizeUpperBoundCheckBox;
+  IBOutlet NSTextField  *sizeUpperBoundField;
+  IBOutlet NSPopUpButton  *sizeUpperBoundUnits;
 
   IBOutlet NSButton  *doneButton;
-
 }
 
 - (IBAction) cancelEdit:(id)sender;
