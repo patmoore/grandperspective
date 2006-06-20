1da2612da16ac84f6b9ba6ea90da63226e630741
erwin <erwin@3e1cca9a-9203-0410-b20e-959a89a76e17>
2006-06-20 19:09:19 +0000

    Added Folder test and various filesize tests to default FileItemTestRepository.
    git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@114 3e1cca9a-9203-0410-b20e-959a89a76e17

--- code/filter/FileItemTestRepository.m
+++ code/filter/FileItemTestRepository.m
@@ -6,6 +6,7 @@
 #import "FileItemTest.h"
 #import "ItemNameTest.h"
 #import "ItemTypeTest.h"
+#import "ItemSizeTest.h"
 #import "CompoundAndItemTest.h"
 
 #import "../util/NotifyingDictionary.h"
@@ -29,7 +30,63 @@ static FileItemTestRepository  *defaultFileItemTestRepository = nil;
     NSMutableDictionary*  initialTestDictionary = 
                              [[NSMutableDictionary alloc] initWithCapacity:32];    
     
-    // TODO: Should get this from user defaults eventually.
+    // TODO: Should get this from user defaults eventually.    
+
+    NSObject <FileItemTest>  *fileTypeTest =
+      [[[ItemTypeTest alloc] initWithTestForPlainFile:YES] autorelease];
+
+    NSObject <FileItemTest>  *folderTypeTest =
+      [[[ItemTypeTest alloc] initWithTestForPlainFile:NO] autorelease];
+    [initialTestDictionary setObject:folderTypeTest forKey:@"Folders"];
+
+    NSObject <FileItemTest>  *tinyFileSizeTest = // 0 - 1k
+      [[[ItemSizeTest alloc] initWithUpperBound:1024] autorelease];
+    NSArray  *tinyFileTests = 
+      [NSArray arrayWithObjects:tinyFileSizeTest, fileTypeTest, nil];
+    NSObject <FileItemTest>  *tinyFileTest = 
+      [[[CompoundAndItemTest alloc] initWithSubItemTests:tinyFileTests] 
+           autorelease];
+    [initialTestDictionary setObject:tinyFileTest forKey:@"Tiny files"];
+    
+    NSObject <FileItemTest>  *smallFileSizeTest = // 1k - 10k
+      [[[ItemSizeTest alloc] initWithLowerBound:1024
+                                     upperBound:10240] autorelease];
+    NSArray  *smallFileTests = 
+      [NSArray arrayWithObjects:smallFileSizeTest, fileTypeTest, nil];
+    NSObject <FileItemTest>  *smallFileTest = 
+      [[[CompoundAndItemTest alloc] initWithSubItemTests:smallFileTests] 
+           autorelease];
+    [initialTestDictionary setObject:smallFileTest forKey:@"Small files"];
+
+    NSObject <FileItemTest>  *mediumFileSizeTest = // 10k - 1M
+      [[[ItemSizeTest alloc] initWithLowerBound:10240
+                                     upperBound:1048576] autorelease];
+    NSArray  *mediumFileTests = 
+      [NSArray arrayWithObjects:mediumFileSizeTest, fileTypeTest, nil];
+    NSObject <FileItemTest>  *mediumFileTest = 
+      [[[CompoundAndItemTest alloc] initWithSubItemTests:mediumFileTests] 
+           autorelease];
+    [initialTestDictionary setObject:mediumFileTest forKey:@"Medium files"];
+
+    NSObject <FileItemTest>  *largeFileSizeTest = // 1M - 100M
+      [[[ItemSizeTest alloc] initWithLowerBound:1048576
+                                     upperBound:104857600] autorelease];
+    NSArray  *largeFileTests = 
+      [NSArray arrayWithObjects:largeFileSizeTest, fileTypeTest, nil];
+    NSObject <FileItemTest>  *largeFileTest = 
+      [[[CompoundAndItemTest alloc] initWithSubItemTests:largeFileTests] 
+           autorelease];
+    [initialTestDictionary setObject:largeFileTest forKey:@"Large files"];
+
+    NSObject <FileItemTest>  *hugeFileSizeTest = // 100M - ...
+      [[[ItemSizeTest alloc] initWithLowerBound:104857600] autorelease];
+    NSArray  *hugeFileTests = 
+      [NSArray arrayWithObjects:hugeFileSizeTest, fileTypeTest, nil];
+    NSObject <FileItemTest>  *hugeFileTest = 
+      [[[CompoundAndItemTest alloc] initWithSubItemTests:hugeFileTests] 
+           autorelease];
+    [initialTestDictionary setObject:hugeFileTest forKey:@"Huge files"];
+
     NSArray  *imageExtensions = 
       [NSArray arrayWithObjects:@".jpg", @".JPG", @".png", @".PNG", @".gif", 
                                 @".GIF", nil];
@@ -39,10 +96,8 @@ static FileItemTestRepository  *defaultFileItemTestRepository = nil;
     NSObject <FileItemTest>  *imageNameTest =
       [[[ItemNameTest alloc] initWithStringTest:imageStringTest]
            autorelease];
-    NSObject <FileItemTest>  *imageTypeTest =
-      [[[ItemTypeTest alloc] initWithTestForPlainFile:YES] autorelease];
     NSArray  *imageTests = 
-      [NSArray arrayWithObjects:imageNameTest, imageTypeTest, nil];
+      [NSArray arrayWithObjects:imageNameTest, fileTypeTest, nil];
     NSObject <FileItemTest>  *imageTest = 
       [[[CompoundAndItemTest alloc] initWithSubItemTests:imageTests] 
            autorelease];
@@ -56,10 +111,8 @@ static FileItemTestRepository  *defaultFileItemTestRepository = nil;
     NSObject <FileItemTest>  *musicNameTest =
       [[[ItemNameTest alloc] initWithStringTest:musicStringTest]
            autorelease];
-    NSObject <FileItemTest>  *musicTypeTest =
-      [[[ItemTypeTest alloc] initWithTestForPlainFile:YES] autorelease];
     NSArray  *musicTests = 
-      [NSArray arrayWithObjects:musicNameTest, musicTypeTest, nil];
+      [NSArray arrayWithObjects:musicNameTest, fileTypeTest, nil];
     NSObject <FileItemTest>  *musicTest = 
       [[[CompoundAndItemTest alloc] initWithSubItemTests:musicTests] 
            autorelease];
@@ -73,11 +126,8 @@ static FileItemTestRepository  *defaultFileItemTestRepository = nil;
     NSObject <FileItemTest>  *versionControlNameTest =
       [[[ItemNameTest alloc] initWithStringTest:versionControlStringTest]
            autorelease];
-    NSObject <FileItemTest>  *versionControlTypeTest =
-      [[[ItemTypeTest alloc] initWithTestForPlainFile:NO] autorelease];
     NSArray  *versionControlTests = 
-      [NSArray arrayWithObjects:versionControlNameTest, versionControlTypeTest, 
-                                nil];
+      [NSArray arrayWithObjects:versionControlNameTest, fileTypeTest, nil];
     NSObject <FileItemTest>  *versionControlTest = 
       [[[CompoundAndItemTest alloc] initWithSubItemTests:versionControlTests]
            autorelease];
