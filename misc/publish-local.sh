1c2ad59a2373b7287ce3784a0fb3d53276e45083
erwin <erwin@3e1cca9a-9203-0410-b20e-959a89a76e17>
2006-12-10 15:38:14 +0000

    Script file for creating localized release.
    git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@259 3e1cca9a-9203-0410-b20e-959a89a76e17

---
+++ misc/publish-local.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Creates the GrandPerspective DMG release file with localized resources.
+# It assumes that the disk image for the basic release file is mounted, as
+# it copies the application directly from it. Release text files and the
+# localized resources are taken from the SVN repository.
+
+VERSION="0.97"
+VERSION_ID="0_97"
+
+TEMP_PARENT_PATH="/Users/erwin/temp"
+TEMP_PATH=`mktemp -d ${TEMP_PARENT_PATH}/publish-local-XXXXXX` || exit -1
+
+echo "Output to" $TEMP_PATH
+
+DEST_PATH=$TEMP_PATH
+APP_DIR="GrandPerspective.app"
+RESOURCES_PATH=$DEST_PATH/$APP_DIR/Contents/Resources
+
+SVN_URL=https://194.121.182.66/svn/erwin/GrandPerspective
+
+OUT_DMG_FILE=GrandPerspective-${VERSION_ID}-local.dmg
+
+echo "Copying basic application."
+tar cf - -C /Volumes/GrandPerspective-${VERSION_ID} $APP_DIR | tar xf - -C $DEST_PATH
+
+echo "Exporting text files."
+./export-docs.sh trunk/docs 256 $DEST_PATH $VERSION $VERSION_ID
+
+for loc in nl de ja
+do
+  echo "Exporting" $loc "resources."
+  svn export -q -r 256 $SVN_URL/trunk/code/${loc}.lproj $RESOURCES_PATH/${loc}.lproj
+done
+
+# Remove unneeded nib files.
+find $RESOURCES_PATH/ \( -name classes.nib -or -name info.nib \) -delete
+
+/Users/Erwin/bin/buildDMG.pl -dmgName ${OUT_DMG_FILE%.dmg} -buildDir $TEMP_PARENT_PATH -volSize 1 -compressionLevel 9 ${DEST_PATH}/*.txt ${DEST_PATH}/${APP_DIR}
+
+rm -rf $TEMP_PATH
+
+
