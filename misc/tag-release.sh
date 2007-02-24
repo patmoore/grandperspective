4d12cd620d4d08ca84981a9a769d6bd4ab0a311b
erwin <erwin@3e1cca9a-9203-0410-b20e-959a89a76e17>
2007-02-24 12:17:20 +0000

    Updated script for tagging Version 0.99 release.
    git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@407 3e1cca9a-9203-0410-b20e-959a89a76e17

--- misc/tag-release.sh
+++ misc/tag-release.sh
@@ -4,18 +4,20 @@
 # Modify as needed before tagging a new release.
 
 SVN_URL=https://194.121.182.66/svn/erwin/GrandPerspective
-VERSION="0.98"
-VERSION_ID="0_98"
+VERSION="0.99"
+VERSION_ID="0_99"
 
 TAG_URL=${SVN_URL}/tags/release-${VERSION_ID}
 
 echo svn mkdir -m \""Creating Version ${VERSION} tag directory."\" $TAG_URL
 
-echo svn copy -r 286 $SVN_URL/trunk/code $TAG_URL/code -m \""Tagging Version ${VERSION} of the code."\"
+echo svn copy -r 385 $SVN_URL/trunk/code $TAG_URL/code -m \""Tagging Version ${VERSION} of the code."\"
 
-echo svn copy -r 286 $SVN_URL/trunk/docs $TAG_URL/docs -m \""Tagging Version ${VERSION} of the release documents."\"
+echo svn copy -r 383 $SVN_URL/trunk/docs $TAG_URL/docs -m \""Tagging Version ${VERSION} of the release documents."\"
 
-for f in export-docs.sh export-source.sh publish.sh
+echo svn copy -r 390 $SVN_URL/trunk/help $TAG_URL/help -m \""Tagging Version ${VERSION} of the help documentation."\"
+
+for f in export-docs.sh export-help.sh export-source.sh publish-source.sh publish-app.sh update-help-index.sh
 do
   echo svn copy $SVN_URL/trunk/misc/$f $TAG_URL/$f -m \""Tagging Version ${VERSION} of the release scripts."\"
 done
