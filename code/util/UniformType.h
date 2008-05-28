af569fed9f76e9f9bbefd6ac40b2fd282a4864ec
eriban <eriban@3e1cca9a-9203-0410-b20e-959a89a76e17>
2008-05-28 20:03:50 +0000

    Extended comments.
    git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@705 3e1cca9a-9203-0410-b20e-959a89a76e17

--- code/util/UniformType.h
+++ code/util/UniformType.h
@@ -3,11 +3,11 @@
 
 // Note: Instances are immutable (and as a result, the class is thread-safe). 
 @interface UniformType : NSObject {
-  NSString*  uti;
-  NSString*  description;
+  NSString  *uti;
+  NSString  *description;
 
   // An immutable set of "UniformType"s
-  NSSet*  parents;
+  NSSet  *parents;
 }
 
 
@@ -23,6 +23,18 @@
 
 /* Dynamically constructs the set of types that the receiving type conforms to
  * (directly or indirectly).
+ *
+ * Conformance of a given type, typeA, to another type, typeB can be tested as
+ * follows:
+ *
+ *   (typeA == typeB) || ([[typeA ancestorTypes] containsObject: typeB])
+ *
+ * The reason that there is no method implementing this test directly is that
+ * for multiple subsequent conformance tests for the same type, which is
+ * typical usage, you should construct the ancestor set only once. This could
+ * easily be forgotten if there would be a -conformsTo: convenience method.
+ * Furthermore, such a method would also hide the execution overhead associated
+ * with conformance tests.
  */
 - (NSSet *)ancestorTypes;
 
