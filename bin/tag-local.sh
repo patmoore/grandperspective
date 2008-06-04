1405bc6424703ebc9c3b8d293803e40e1ed501a4
eriban <eriban@3e1cca9a-9203-0410-b20e-959a89a76e17>
2008-06-04 18:46:42 +0000

    Updated for tagging Version 0.9.11 (Localized).
    git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@768 3e1cca9a-9203-0410-b20e-959a89a76e17

--- bin/tag-local.sh
+++ bin/tag-local.sh
@@ -9,18 +9,26 @@ then
   exit -1
 fi
 
-VERSION="0.9.10 (Localized)"
-VERSION_ID="0_9_10-local"
+VERSION="0.9.11 (Localized)"
+VERSION_ID="0_9_11-local"
+
+REV=667
+BIN_REV=668
 
 TAG_URL=${GP_SVN_URL}/tags/release-${VERSION_ID}
 
 echo svn mkdir -m \""Creating Version ${VERSION} tag directory."\" $TAG_URL
 
-echo svn copy -r 556 $GP_SVN_URL/trunk/docs $TAG_URL/docs -m \""Tagging Version ${VERSION} of the release documents."\"
+echo svn copy -r $REV $GP_SVN_URL/trunk/docs $TAG_URL/docs -m \""Tagging Version ${VERSION} of the release documents."\"
 
-echo svn copy -r 555 $GP_SVN_URL/trunk/locs $TAG_URL/locs -m \""Tagging Version ${VERSION} of the third-party localizations."\"
+echo svn mkdir -m \""Creating Version ${VERSION} tag/locs directory."\" $TAG_URL/locs
+
+for loc in ja
+do
+  echo svn copy -r $REV $GP_SVN_URL/trunk/locs/${loc}.lproj $TAG_URL/locs/${loc}.lproj -m \""Tagging Version ${VERSION} of the third-party localizations."\"
+done
 
 for f in publish-local.sh export-docs.sh export-help.sh update-help-index.sh
 do
-  echo svn copy -r 557 $GP_SVN_URL/trunk/bin/$f $TAG_URL/$f -m \""Tagging Version ${VERSION} of the release scripts."\"
+  echo svn copy -r $BIN_REV $GP_SVN_URL/trunk/bin/$f $TAG_URL/$f -m \""Tagging Version ${VERSION} of the release scripts."\"
 done
