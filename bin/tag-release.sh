b1d57643ca8569bd02843086b0d25b11db515fd6
eriban <eriban@3e1cca9a-9203-0410-b20e-959a89a76e17>
2008-06-14 18:23:15 +0000

    Updated for tagging Version 0.9.12.
    git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@823 3e1cca9a-9203-0410-b20e-959a89a76e17

--- bin/tag-release.sh
+++ bin/tag-release.sh
@@ -9,20 +9,22 @@ then
   exit -1
 fi
 
-VERSION="0.9.11"
-VERSION_ID="0_9_11"
+VERSION="0.9.12"
+VERSION_ID="0_9_12"
+SRC_REV=798
+BIN_REV=804
 
 TAG_URL=${GP_SVN_URL}/tags/release-${VERSION_ID}
 
 echo svn mkdir -m \""Creating Version ${VERSION} tag directory."\" $TAG_URL
 
-echo svn copy -r 653 $GP_SVN_URL/trunk/code $TAG_URL/code -m \""Tagging Version ${VERSION} of the code."\"
+echo svn copy -r $SRC_REV $GP_SVN_URL/trunk/code $TAG_URL/code -m \""Tagging Version ${VERSION} of the code."\"
 
-echo svn copy -r 652 $GP_SVN_URL/trunk/docs $TAG_URL/docs -m \""Tagging Version ${VERSION} of the release documents."\"
+echo svn copy -r $SRC_REV $GP_SVN_URL/trunk/docs $TAG_URL/docs -m \""Tagging Version ${VERSION} of the release documents."\"
 
-echo svn copy -r 652 $GP_SVN_URL/trunk/help $TAG_URL/help -m \""Tagging Version ${VERSION} of the help documentation."\"
+echo svn copy -r $SRC_REV $GP_SVN_URL/trunk/help $TAG_URL/help -m \""Tagging Version ${VERSION} of the help documentation."\"
 
-for f in export-docs.sh export-help.sh export-source.sh publish-source.sh publish-app.sh update-help-index.sh
+for f in export-docs.sh export-help.sh export-source.sh publish-source.sh publish-app.sh update-help-index.sh buildDMG.pl
 do
-  echo svn copy -r 655 $GP_SVN_URL/trunk/bin/$f $TAG_URL/$f -m \""Tagging Version ${VERSION} of the release scripts."\"
+  echo svn copy -r $BIN_REV $GP_SVN_URL/trunk/bin/$f $TAG_URL/$f -m \""Tagging Version ${VERSION} of the release scripts."\"
 done
