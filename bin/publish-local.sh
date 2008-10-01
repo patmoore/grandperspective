b249b33ae73467c83f8ed8622cafd505c194cf54
eriban <eriban@3e1cca9a-9203-0410-b20e-959a89a76e17>
2008-10-01 21:35:36 +0000

    Updated script for 0.9.13.1 (localized) release.
    git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@957 3e1cca9a-9203-0410-b20e-959a89a76e17

--- bin/publish-local.sh
+++ bin/publish-local.sh
@@ -5,10 +5,10 @@
 # it copies the application directly from it. Release text files and the
 # localized resources are taken from the SVN repository.
 
-VERSION="0.9.11"
-VERSION_ID="0_9_11"
-REV=667
-NL_REV=667
+VERSION="0.9.13.1"
+VERSION_ID="0_9_13_1"
+REV=955
+NL_REV=955
 
 if [ $# -ne "1" ]
 then
@@ -46,13 +46,16 @@ tar cf - -C /Volumes/GrandPerspective-${VERSION_ID} $APP_FOLDER | tar xf - -C $T
 echo "Exporting text files."
 ./export-docs.sh trunk/docs $REV $TEMP_PUBLISH_DIR $VERSION $VERSION_ID
 
+echo "Exporting credits."
+svn export -q -r $REV $GP_SVN_URL/trunk/code/Credits.rtf $RESOURCES_PATH/Credits.rtf
+
 echo "Exporting nl resources."
 svn export -q -r $NL_REV $GP_SVN_URL/trunk/code/nl.lproj $RESOURCES_PATH/nl.lproj
 
 echo "Exporting nl help documentation."
 ./export-help.sh $NL_REV $RESOURCES_PATH nl
 
-for loc in ja
+for loc in ja fr es
 do
   echo "Exporting" $loc "resources."
   svn export -q -r $REV $GP_SVN_URL/trunk/locs/${loc}.lproj $RESOURCES_PATH/${loc}.lproj
@@ -61,25 +64,16 @@ done
 echo "Updating nl help index."
 ./update-help-index.sh $RESOURCES_PATH/nl.lproj/$HELP_FOLDER European
 
-echo -n "[Please press return once the indexing is done]" 
-read dummy
+echo "Updating fr help index."
+./update-help-index.sh $RESOURCES_PATH/fr.lproj/$HELP_FOLDER European
 
-#echo "Updating fr help index."
-#./update-help-index.sh $RESOURCES_PATH/fr.lproj/$HELP_FOLDER European
-
-#echo -n "[Please press return once the indexing is done]" 
-#read dummy
-
-#echo "Updating es help index."
-#./update-help-index.sh $RESOURCES_PATH/es.lproj/$HELP_FOLDER European
-
-#echo -n "[Please press return once the indexing is done]" 
-#read dummy
+echo "Updating es help index."
+./update-help-index.sh $RESOURCES_PATH/es.lproj/$HELP_FOLDER European
 
 # Remove unneeded nib files.
 find $RESOURCES_PATH/ \( -name classes.nib -or -name info.nib \) -delete
 
-/Users/Erwin/bin/buildDMG.pl -dmgName ${OUT_DMG_FILE%.dmg} -buildDir $DEST_PATH -volSize 2 -compressionLevel 9 ${TEMP_PUBLISH_DIR}/*.txt ${TEMP_PUBLISH_DIR}/${APP_FOLDER}
+/Users/Erwin/bin/buildDMG.pl -dmgName ${OUT_DMG_FILE%.dmg} -buildDir $DEST_PATH -volSize 3 -compressionLevel 9 ${TEMP_PUBLISH_DIR}/*.txt ${TEMP_PUBLISH_DIR}/${APP_FOLDER}
 
 rm -rf $TEMP_PUBLISH_DIR
 
