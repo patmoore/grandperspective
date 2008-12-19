patmoore/grandperspective

commit 2d61022c9f99b113da2a8eea53c0e144dc50deae

Focus shortcuts now also require the Command key (and have become key equivalents) to match keyboard shortcuts for zooming.
git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@1038 3e1cca9a-9203-0410-b20e-959a89a76e17

code/view/DirectoryView.m
@@ -292,27 +292,28 @@ NSString  *ColorMappingChangedEvent = @"colorMappingChanged";
 }
 
 
-- (void) keyDown: (NSEvent *)theEvent {
-  NSString*  chars = [theEvent characters];
-  if ([chars isEqualToString: @"]"]) {
-    if ([self canMoveFocusDown]) {
-      [self moveFocusDown];
-    }
-  }
-  else if ([chars isEqualToString: @"["]) {
-    if ([self canMoveFocusUp]) {
-      [self moveFocusUp];
-    }
-  }
-}
-
-
 - (BOOL)performKeyEquivalent: (NSEvent *)theEvent {
   int  flags = [theEvent modifierFlags] & NSDeviceIndependentModifierFlagsMask;
   NSString  *chars = [theEvent characters];
   
-  if ([chars isEqualToString: @"="]) {
-    if (flags == (NSCommandKeyMask | NSShiftKeyMask)) {
+  if ([chars isEqualToString: @"]"]) {
+    if (flags == NSCommandKeyMask) {
+      if ([self canMoveFocusDown]) {
+        [self moveFocusDown];
+      }
+    }
+  }
+  else if ([chars isEqualToString: @"["]) {
+    if (flags == NSCommandKeyMask) {
+      if ([self canMoveFocusUp]) {
+        [self moveFocusUp];
+      }
+    }
+  }
+  else if ([chars isEqualToString: @"="]) {
+    // Accepting this with or without the Shift key-pressed, as having to use 
+    // the Shift key is a bit of a pain.
+    if ((flags | NSShiftKeyMask) == (NSCommandKeyMask | NSShiftKeyMask)) {
       if ([self canZoomIn]) {
         [self zoomIn];
       }

code/control/DirectoryViewToolbarControl.m
@@ -282,8 +282,8 @@ NSMutableDictionary  *createToolbarItemLookup = nil;
   // help to make the user aware of them or remind the user of them). They do
   // not actually have an effect. Handling these key equivalents is handled in
   // the DirectoryView class. 
-  [zoomInItem setKeyEquivalent: @"-"];
-  [zoomInItem setKeyEquivalentModifierMask: NSCommandKeyMask];
+  [zoomOutItem setKeyEquivalent: @"-"];
+  [zoomOutItem setKeyEquivalentModifierMask: NSCommandKeyMask];
   
   [zoomInItem setKeyEquivalent: @"+"];
   [zoomInItem setKeyEquivalentModifierMask: NSCommandKeyMask];
@@ -323,8 +323,20 @@ NSMutableDictionary  *createToolbarItemLookup = nil;
 
   ToolbarItemMenu  *menu = 
     [[[ToolbarItemMenu alloc] initWithTitle: title target: self] autorelease];
-  [menu addAction: @selector(moveFocusUp:) withTitle: moveUpTitle];
-  [menu addAction: @selector(moveFocusDown:) withTitle: moveDownTitle];
+  NSMenuItem  *focusUpItem = 
+    [menu addAction: @selector(moveFocusUp:) withTitle: moveUpTitle];
+  NSMenuItem  *focusDownItem =
+    [menu addAction: @selector(moveFocusDown:) withTitle: moveDownTitle];
+  
+  // Set the key equivalents so that they show up in the menu (which may
+  // help to make the user aware of them or remind the user of them). They do
+  // not actually have an effect. Handling these key equivalents is handled in
+  // the DirectoryView class. 
+  [focusUpItem setKeyEquivalent: @"["];
+  [focusUpItem setKeyEquivalentModifierMask: NSCommandKeyMask];
+  
+  [focusDownItem setKeyEquivalent: @"]"];
+  [focusDownItem setKeyEquivalentModifierMask: NSCommandKeyMask];
 
   [item setMenuFormRepresentation: menu];
 

commit 03a4d1b95617253d5e9e63069da906ac2e7e85c1

Fix of minor bug introduced in r1038.
git-svn-id: https://grandperspectiv.svn.sourceforge.net/svnroot/grandperspectiv/GrandPerspective/trunk@1046 3e1cca9a-9203-0410-b20e-959a89a76e17

code/view/DirectoryView.m
@@ -301,6 +301,7 @@ NSString  *ColorMappingChangedEvent = @"colorMappingChanged";
       if ([self canMoveFocusDown]) {
         [self moveFocusDown];
       }
+      return YES;
     }
   }
   else if ([chars isEqualToString: @"["]) {
@@ -308,6 +309,7 @@ NSString  *ColorMappingChangedEvent = @"colorMappingChanged";
       if ([self canMoveFocusUp]) {
         [self moveFocusUp];
       }
+      return YES;
     }
   }
   else if ([chars isEqualToString: @"="]) {